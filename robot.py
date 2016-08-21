c477fae5f5305d9b2d9e954e62045ec365d57a7f
pa7lux <pa7lux@gmail.com>
2016-08-21 13:14:45 +0300

    добавил скрипт  робота

---
+++ robot.py
@@ -0,0 +1,76 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+import time
+pos = mc.player.getTilePos()
+x,y,z = pos.x,pos.y,pos.z
+u = 35,14#head-up
+h = 35#head-down
+b = 57#breast
+p = 1#press
+a = 41#arms
+ll = 89#legs
+s = 35#skin
+o = 0#air
+veryBigArray = [
+    [
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        ],
+        [
+        [o,o,o,u,u,o,o,o],
+        [o,o,o,h,h,o,o,o],
+        [a,a,b,b,b,b,a,a],
+        [a,a,b,b,b,b,a,a],
+        [a,a,p,p,p,p,a,a],
+        [s,s,p,p,p,p,s,s],
+        [o,o,p,p,p,p,o,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o]
+        ],
+        [
+        [o,o,o,u,u,o,o,o],
+        [o,o,o,h,h,o,o,o],
+        [a,a,b,b,b,b,a,a],
+        [a,a,b,b,b,b,a,a],
+        [a,a,p,p,p,p,a,a],
+        [s,s,p,p,p,p,s,s],
+        [o,o,p,p,p,p,o,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o],
+        [o,ll,ll,o,o,ll,ll,o]
+        ],
+        [
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,b,b,b,b,o,o],
+        [o,o,b,b,b,b,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,p,p,p,p,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o],
+        [o,o,o,o,o,o,o,o]
+        ]
+        ]
+for letter in veryBigArray:
+    y = pos.y
+    for level in letter:
+        x = pos.x
+        for block in level:
+            mc.setBlock(x,y,z,block)
+            x+=1
+        y-=1
+    z-=1
